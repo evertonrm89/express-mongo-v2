52dab75c745450d25977290fadb234695e8dfa6a
evertonrm89 <evertonrm89@gmail.com>
2025-12-05 00:05:30 -0300

    casa - 04/12

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 
-mongoose.connect("mongodb+srv://alura:123@alura.dkjed.mongodb.net/alura-node");
+mongoose.connect('mongodb+srv://admin:admin123@cluster0.gtns9g8.mongodb.net/livraria?appName=Cluster0');
 
 let db = mongoose.connection;
 
