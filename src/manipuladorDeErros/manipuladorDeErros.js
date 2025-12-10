6b9ad7eb785105eed8064b229f78acd0e72ad147
Imac-loja <evertonrm89@gmail.com>
2025-12-10 20:19:28 -0300

    loja - 10/12

--- src/manipuladorDeErros/manipuladorDeErros.js
+++ src/manipuladorDeErros/manipuladorDeErros.js
@@ -2,9 +2,21 @@ import mongoose from "mongoose";
 
 // eslint-disable-next-line no-unused-vars
 function manipuladorDeErros(error, req, res, next) {
+
+  console.log(error);
+
   if (error instanceof mongoose.Error.CastError) {
     res.status(400).send({message: "Um ou mais dados fornecidos estão incorretos."});
-    } else{
+    }else if(error instanceof mongoose.Error.ValidationError){
+
+      const mensagemErro = Object.values(error.Error)
+        .map(error => error.message)
+        .join("; ");
+
+      res.status(400).send({message: "Houve um erro de validação de dados"});
+    } 
+    
+    else{
         res.status(500).send({message: "Erro Interno do Servidor."});
     }
 }
