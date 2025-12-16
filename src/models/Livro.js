690c709059b2f7a9f68e57dd2018b95e661b1bae
evertonrm89 <evertonrm89@gmail.com>
2025-12-16 08:27:09 -0300

    casa - 15/12

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -2,22 +2,42 @@ import mongoose from "mongoose";
 
 const livroSchema = new mongoose.Schema(
   {
-    id: {type: String},
+    id: { type: String },
     titulo: {
-      type: String, 
-      required: [true, "O título do livro é obrigatório"]},
+      type: String,
+      required: [true, "O título do livro é obrigatório"]
+    },
     editora: {
-      type: String, 
-      required: [true, "A editora do livro é obrigatória"]},
-    numeroPaginas: {type: Number},
-    preco: {type: Number},
+      type: String,
+      required: [true, "A editora do livro é obrigatória"],
+      enum: {
+        values: ["Casa do Código", "Novatec", "Alura", "Outros"],
+        message: "A editora {VALUE} não é um valor permitido "
+
+      }
+    },
+    numeroPaginas: {
+      type: Number,
+      min: [10, "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"],
+      max: [5000, "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"]
+    },
+    preco: {
+      type: Number,
+      validate: {
+        validator: (valor) => {
+          return valor >= 1;
+        },
+        message: "O preço do livro deve ser maior ou igual a R$ 1,00. Valor fornecido: {VALUE}"
+      }
+    },
     autor: {
-      type: mongoose.Schema.Types.ObjectId, 
-      ref: 'autores', 
-      required: [true, "O autor do livro é obrigatório."]}
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'autores',
+      required: [true, "O autor do livro é obrigatório."]
+    }
   }
 );
 
-const livros= mongoose.model('livros', livroSchema);
+const livros = mongoose.model('livros', livroSchema);
 
 export default livros;
